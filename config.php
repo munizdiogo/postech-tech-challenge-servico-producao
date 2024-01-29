b8f64ee1dc4fe9b856ae385024d76ee51848699d
Diogo Muniz <diogomuniz.web@gmail.com>
2024-01-29 02:44:05 -0300

    Alteração dos dados de conexão

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-const DB_HOST = "localhost";
+const DB_HOST = "dbpostech.cd6yk7z7kstc.us-east-1.rds.amazonaws.com";
 const DB_NAME = "dbpostech";
-const DB_USERNAME = "root";
-const DB_PASSWORD = "";
-const DB_PORT = 3306; 
+const DB_USERNAME = "admin";
+const DB_PASSWORD = "Postech123";
+const DB_PORT = 3306;
